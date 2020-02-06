62bcc85f22e8f88939fb19574551c5bb536518b9
Anthony Valenzi <anthonymv1313@gmail.com>
2020-02-06 09:27:03 -0700

    created if part of if/else statements push

--- script.js
+++ script.js
@@ -6,13 +6,68 @@ var generateBtn = document.querySelector("#generate");
 // Write password to the #password input
 function writePassword() {
   var params = {
-    length: prompt("How long would you like your password to be?"),
+    length: prompt("How long would you like your password to be?"),   
     lowercase: confirm("Would you like to use lowercase characters?"),
     uppercase: confirm("Would you like to use uppercase characters?"),
     specialChars: confirm("Would you like to use special characters?"),
     numericChars: confirm("Would you like to use numeric characters?")
   };
 ​
+var userInput = parseInt(prompt(
+  ));
+function userLength(){
+
+  // Conditional statement to check if password length is a number. Prompts end if this evaluates false
+  if (isNaN(userInput) === true) {
+      alert("Password length must be provided as a number");
+      return;
+  }
+
+  // Conditional statement to check if password length is at least 8 characters long. Prompts end if this evaluates false
+  if (userInput < 8) {
+      alert("Password length must be at least 8 characters");
+      return;
+  }
+
+  // Conditional statement to check if password length is less than 128 characters long. Prompts end if this evaluates false
+  if (userInput > 128) {
+      alert("Password length must less than 129 characters");
+      return;
+  }
+}
+
+function lowerCase(){
+
+  // Conditional statement to check if password is lowercase. Prompts end if this evaluates false
+  if (islowerCase(userInput) === true) {
+      alert("Password must contain lowercase characters");
+      return;
+  }
+  function upperCase(){
+
+// Conditional statement to check if password is Uppercase. Prompts end if this evaluates false
+  if (isupperCase(userInput) === true) {
+     alert("Password must contain uppercase characters");
+    return;
+
+  function specialChars(){
+
+// Conditional statement to check if password has special characters. Prompts end if this evaluates false
+  if (specialChars(userInput) === true) {
+      alert("Password must contain special characters");
+        return;
+
+  function numericChars(){
+
+// Conditional statement to check if password is numeric characters. Prompts end if this evaluates false
+  if (numericChars(userInput) === true) {
+      alert("Password must contain numeric characters");
+            return;
+     
+ 
+}
+
+
 const aNumber = Number(window.prompt("Type a number", ""))
 
   var password = generatePassword(params);
@@ -32,12 +87,3 @@ function generatePassword(options) {
 generateBtn.addEventListener("click", writePassword);
 
 
-function myPassword() {
-  var text;
-  var length = prompt("How long would you like your password to be?", "7");
-  switch(length) 
-    break;
-  var lowerCase = prompt("Would you like to use lowercase characters", "yes");
-    switch(lowerCase)
-   document.getElementById("example").innerHTML = text;
- }
